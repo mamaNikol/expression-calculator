79123b0f770507ab595b0984039f110b68ac9bf2
mamaNikol <49046890+mamaNikol@users.noreply.github.com>
2020-03-10 01:09:42 +0900

    feat: start

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+function expressionCalculator(str) {
+    let arr;
+    if(typeof str === 'string'){
+     arr = tostring(str);
+    } else arr = str
+      let n = 0;
+      let m = 0;
+    for (let i = 0; i < arr.length; i++){
+      if(arr[i] === '('){
+        n += 1;
+      }
+      if(arr[i] === ')'){
+        m += 1;
+      }
+    }
+    
 }
 
 module.exports = {
